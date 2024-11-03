b4b08087fc8b9996f2d20c2fa82ba0b5f286a6a7
robin <robincharles940@gmail.com>
2024-11-03 15:51:47 +0300

    HOMES: Routing yet to be dynamic

--- homes-app/src/app/app.component.ts
+++ homes-app/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, RouterModule],
   template: `
-    <h1>Hellow world!</h1>
-  `,
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/homes-logo.png" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `
 })
 export class AppComponent {
   title = 'homes';
